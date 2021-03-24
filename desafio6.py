ac6ce68b5b3534abcbe9ee9114136414da527ea7
Lucas Pereira <80165843+lmdpadm@users.noreply.github.com>
2021-03-23 21:04:58 -0300

    Delete desafio6.py

--- desafio6.py
+++
@@ -1,6 +0,0 @@
-n = int(input('Escolha um algoritmo:'))
-#d = n * 2
-#t = n * 3
-#r = n ** (1/2)
-#print('O dobro do número {} é {}, o triplo é {} e a raíz quadrada é {:.5f}'.format(n, d, t, r))
-print('O dobro do número {} é {}, \nO triplo é {}, \nE a raíz quadrada é {:.5f}.'.format(n, (n*2), (n*3), (n**0.5)))
