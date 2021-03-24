6692a46ffbeab4df93fe7f1e7eb7a55722aa90f3
Lucas Pereira <80165843+lmdpadm@users.noreply.github.com>
2021-03-23 21:05:03 -0300

    Delete desafio7.py

--- desafio7.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input('A nota da P1 de Lucas foi:'))
-n2 = float(input('A nota da P2 de Lucas foi:'))
-#m = (n1 + n2) / 2
-#print('Lucas tirou na P1 a nota {}, na P2 a nota {}, então ficou com a média de {:.2f}'.format(n1, n2, m))
-print('Lucas tirou na P1 a nota {}, na P2 a nota {}, então ficou com a média de {:.2f}.'.format(n1, n2, ((n1+n2)/2)))
