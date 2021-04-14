febffa6c5551a6f04517bcecab7675fd8552a6c3
Lucas Pereira <80165843+lmdpadm@users.noreply.github.com>
2021-04-13 21:05:30 -0300

    Add files via upload

---
+++ desafio46.py
@@ -0,0 +1,7 @@
+import time
+print('\033[1;33m{}\033[m \033[1;31mCONTAGEM REGRESSIVA PARA O ANO NOVO \033[1;33m{}\033[m'.format('✯' * 5, '✯' * 5))
+for c in range(10, 0, - 1):
+    time.sleep(1)
+    print(c)
+    time.sleep(1)
+print('\033[1;34m✯✯✯✯✯ FELIZ ANO NOVO ✯✯✯✯✯\033[m')
